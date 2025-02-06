168f5922c056b6b051a3e9f2589efe05707d41d9
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-02-06 17:38:16 +0200

    Added linter and entry point

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
+import printMessage from "./src/index";
 
-console.log('Hello, Hexlet!');
-console.log(_.last(['one', 'two']))
+printMessage();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from "lodash";
+
+const printMessage = () => {
+  console.log("Hello, Hexlet!");
+  console.log(_.last(["one", "two"]));
+};
+
+export default printMessage;
